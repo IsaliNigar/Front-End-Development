35ccfa06d869cfdf81e32eadf68838e3cc066788
IsaliNigar <122671961+IsaliNigar@users.noreply.github.com>
2023-05-21 15:10:52 +0400

    Add files via upload

--- Java_Bootstrap_accordion/script.js
+++ Java_Bootstrap_accordion/script.js
@@ -1,9 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var toggle = document.getElementsByClassName('toggle')[0];
-  
-    toggle.addEventListener('click', function() {
-      var content = toggle.nextElementSibling;
-      content.classList.toggle('show');
-    });
-  });
-  
+fetch('https://reqres.in/api/users?page=2')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+var posts = '';
+data.data.forEach(element => {
+    posts += `
+    
+    <div class="card" style="width: 18rem;">
+  <img src="${element.avatar}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="${element.email}">${element.email}</h5>
+    <p class="${element.first_name}">${element.first_name} ${element.last_name}</p>
+  </div>
+</div>
+    ` 
+});
+
+document.querySelector(".postRow").innerHTML=posts
+  })
